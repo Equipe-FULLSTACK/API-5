a6daf82aad7e19a8e325ea5f3bc6401f432855e1
Juliano Prado <125655127+julianopradoo@users.noreply.github.com>
2024-09-16 05:19:27 -0300

    Update FisicoCreateV1.0.sql

--- Backend/Database/Relacional/Fisico/FisicoCreateV1.0.sql
+++ Backend/Database/Relacional/Fisico/FisicoCreateV1.0.sql
@@ -12,30 +12,30 @@ CREATE TABLE ponto (
 	id_ponto INT PRIMARY KEY NOT NULL,
 	id_usuario INT NOT NULL,
 	apelido VARCHAR(256),
-    historico VARCHAR(256),
-    lat_long INT NOT NULL,
-    FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)
+    	historico VARCHAR(256),
+    	lat_long INT NOT NULL,
+   	 FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)
 );
 
 CREATE TABLE notificacao (
 	id_notificacao INT PRIMARY KEY NOT NULL,
-    id_usuario INT NOT NULL,
-    id_ponto INT NOT NULL,
-    data_hora TIMESTAMP,
-    mensagem VARCHAR(256),
-    FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario),
-    FOREIGN KEY (id_ponto) REFERENCES ponto(id_ponto)
+    	id_usuario INT NOT NULL,
+    	id_ponto INT NOT NULL,
+   	 data_hora TIMESTAMP,
+    	mensagem VARCHAR(256),
+    	FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario),
+    	FOREIGN KEY (id_ponto) REFERENCES ponto(id_ponto)
 );
 
 CREATE TABLE relatorio (
 	id_relatorio INT PRIMARY KEY NOT NULL,
-    id_notificacao INT NOT NULL,
-    id_usuario INT NOT NULL,
-    id_ponto INT NOT NULL,
-    data_hora TIMESTAMP,
-    titulo VARCHAR (256),
-    conclusao VARCHAR(256),
+   	 id_notificacao INT NOT NULL,
+   	 id_usuario INT NOT NULL,
+    	id_ponto INT NOT NULL,
+    	data_hora TIMESTAMP,
+   	 titulo VARCHAR (256),
+   	 conclusao VARCHAR(256),
 	FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario),
-    FOREIGN KEY (id_ponto) REFERENCES ponto(id_ponto),
-    FOREIGN KEY (id_notificacao) REFERENCES notificacao(id_notificacao)
+    	FOREIGN KEY (id_ponto) REFERENCES ponto(id_ponto),
+   	 FOREIGN KEY (id_notificacao) REFERENCES notificacao(id_notificacao)
 )
